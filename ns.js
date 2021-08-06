97d26411f52f7018455abe8f625e7f262956a6fb
iTzCrutchie <github@crutchie.com>
2021-08-06 11:16:48 -0500

    Update ns.js

--- ns.js
+++ ns.js
@@ -2,10 +2,10 @@ module.exports = {
     cname: {
         "fsndocs.rundocs.io": "itzcrutchie.github.io/fsndocs",
         /* start */
-        "jekyll-theme-docsify": "itzcrutchie.github.io/fsndocs",
-        "jekyll-rtd-theme": "itzcrutchie.github.io/fsndocs",
+        "jekyll-theme-docsify": "rundocs.github.io",
+        "jekyll-rtd-theme": "rundocs.github.io",
     },
     txt: {
-        "fsndocs.rundocs.io": "google-site-verification=X-KVlK1E2ZRkqgO3UzHYn4Fm26gUlZY132HREO4lkmM",
+        "rundocs.io": "google-site-verification=X-KVlK1E2ZRkqgO3UzHYn4Fm26gUlZY132HREO4lkmM",
     }
 }
