e615a9bad1b5a49ff909776fad371a0bb1861ebc
iTzCrutchie <github@crutchie.com>
2021-08-06 11:15:18 -0500

    Create ns.js

---
+++ ns.js
@@ -0,0 +1,11 @@
+module.exports = {
+    cname: {
+        "fsndocs.rundocs.io": "itzcrutchie.github.io/fsndocs",
+        /* start */
+        "jekyll-theme-docsify": "itzcrutchie.github.io/fsndocs",
+        "jekyll-rtd-theme": "itzcrutchie.github.io/fsndocs",
+    },
+    txt: {
+        "fsndocs.rundocs.io": "google-site-verification=X-KVlK1E2ZRkqgO3UzHYn4Fm26gUlZY132HREO4lkmM",
+    }
+}
